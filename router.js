79e18dbe71f1d9bab7cab7eef5e89028ccce8a78
Jim <wrench1138@gmail.com>
2015-06-12 11:38:06 -0700

    add code to routes files

--- router.js
+++ router.js
@@ -1,4 +1,4 @@
-QAandA.Router.map(function() {
+QAndA.Router.map(function() {
   this.resource('questions', {path: '/'});
   this.resource('new-question');
   this.resource('question', {path: 'questions/:question_id'}, function() {
